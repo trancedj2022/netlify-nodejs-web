20d4ea22ad253075e80f193d40df8e8ca1eec9dd
trancedj2022 <134460924+trancedj2022@users.noreply.github.com>
2025-03-11 14:41:01 +0800

    Update start-with-tunnel.js

--- start-with-tunnel.js
+++ start-with-tunnel.js
@@ -1,7 +1,7 @@
 // 设置环境变量
-process.env.ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'my-poetry-app';
+process.env.ARGO_DOMAIN = process.env.ARGO_DOMAIN || '';
 process.env.ARGO_AUTH = process.env.ARGO_AUTH || ''; // 在实际使用时需要设置此值
-process.env.ARGO_PORT = process.env.ARGO_PORT || '3000'; // 添加端口变量
+process.env.ARGO_PORT = process.env.ARGO_PORT || ''; // 添加端口变量
 
 // 启动应用
 require('./server');
