9bd60af5beaa75c8b3c7235ce732eba9e9164628
trancedj2022 <134460924+trancedj2022@users.noreply.github.com>
2025-03-11 15:00:53 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.ARGO_PORT || process.env.PORT || 3000;
+const port = process.env.PORT || process.env.PORT || 3000;
 
 // 提供静态文件
 app.use(express.static(path.join(__dirname, 'public')));
